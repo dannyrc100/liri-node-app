591ed646e650c1fd02a1d5d772aae1d6f03ebef8
Danny10X <dannyrc100@hotmail.com>
2018-01-17 23:29:38 -0500

    Work Completed

--- keys.js
+++ keys.js
@@ -1,10 +1,23 @@
 console.log('this is loaded');
 
 var twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'eFQjYujVwyIZ1sbiytDAPwEM3',
+  consumer_secret: 'U5BGPqE8XnzTmhxbXxX3Ck8Gx8y5rUVifbiWLtmat2H4FEMjuU',
+  access_token_key: '163937461-RayeDyUn2pTs3gdXWL1UOLGDJHxRvF2JhBPnoz1s',
+  access_token_secret: 'rTzPlcSXuY0VVwF8TrVFgSloNNEysKSRO3aDFrexcLGge'
 };
 
-module.exports = twitterKeys;
+var spotifyKeys = {
+  id: 'ae28c1cb30bf436fa6cc57926862f8ba',
+  secret: 'a9fd86712a134a27b98ff51f6c4ae8c6'
+};
+
+var imdbKeys = {
+  key: '40e9cece'
+};
+
+module.exports = {
+  twitterKeys,
+  spotifyKeys,
+  imdbKeys
+};
---
+++ liri.js
@@ -0,0 +1,150 @@
+var fs = require('fs');
+var requests = require('request');
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
+var keyChain = require('./keys');
+
+var twitterClient = new Twitter(keyChain.twitterKeys);
+var spotifyClient = new Spotify(keyChain.spotifyKeys);
+
+function processCommand(pCommand, pInput) {
+  switch (pCommand) {
+    case 'my-tweets':
+      // show last 20 tweets and their creation time
+      getMyTweets();
+      break;
+    case 'post-tweet':
+      // post new status based on passed argument
+      postMyTweet(pInput);
+      break;
+    case 'spotify-this-song':
+      searchSongAPI(pInput);
+      break;
+    case 'movie-this':
+      searchIMDB(pInput);
+      break;
+    case 'do-what-it-says':
+      doWhatIsSays();
+      break;
+    default:
+      console.log('No matching command!');
+      break;
+  }
+}
+
+function getMyTweets() {
+  twitterClient.get('statuses/user_timeline.json', {
+      count: 20
+    },
+    function (err, tweets, response) {
+      if (err) throw err;
+
+      if (response.statusCode === 200) {
+        console.log('--------------');
+        tweets.forEach(function (t) {
+          console.log(` > ${t.created_at} | ${t.text}`);
+        });
+        console.log('--------------');
+      }
+    });
+}
+
+function postMyTweet(pTweet) {
+  if (process.argv[3]) {
+    twitterClient.post('statuses/update', {
+        status: pTweet
+      },
+      function (err, tweet, response) {
+        if (err) {
+          console.log(JSON.stringify(err));
+        }
+
+        if (response.statusCode === 200) {
+          console.log('--------------');
+          console.log('Posted: ' + pTweet);
+          console.log('--------------');
+        }
+      });
+  } else {
+    console.log('Please enter a String after post-tweet command!');
+  }
+}
+
+function searchSongAPI(pSong) {
+  var apiURL = 'https://api.spotify.com/v1/search?limit=3&type=track&q=';
+  var query = pSong.replace(/ /g, '%20');
+
+  spotifyClient
+    .request(apiURL + query)
+    .then(function (response) {
+      var responseItems = response.tracks.items;
+
+      if (responseItems.length > 0) {
+        responseItems.forEach(function (pEach) {
+          var responseArtists = pEach.artists;
+          var allArtists = '';
+
+          responseArtists.forEach(function (pArtist) {
+            allArtists += (pArtist.name + ', ');
+          });
+
+          console.log('--------------');
+          console.log('Artist(s): ' + allArtists);
+          console.log('Song Name: ' + pEach.name);
+          console.log('Preview URL: ' + pEach.preview_url);
+          console.log('Album Name: ' + pEach.album.name);
+        });
+      } else {
+        searchSongAPI('The Sign');
+      }
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
+
+function searchIMDB(pMovie) {
+  if (pMovie === undefined) {
+    pMovie = 'Mr. Nobody';
+  }
+
+  var query = pMovie.replace(/ /g, '%20');
+  var apiURL = 'http://www.omdbapi.com/?apikey=' + keyChain.imdbKeys.key +
+    '&type=movie&t=' + query;
+
+  requests(apiURL, function (err, response, body) {
+    if (err) {
+      console.log(err);
+      return;
+    } else {
+      var data = JSON.parse(body);
+
+      var jsonOMDB = JSON.parse(body);
+
+      console.log("Title: " + jsonOMDB.Title);
+      console.log("Year: " + jsonOMDB.Year);
+      console.log("Rated: " + jsonOMDB.Rated);
+      console.log("IMDB Rating: " + jsonOMDB.imdbRating);
+      console.log("Country: " + jsonOMDB.Country);
+      console.log("Language: " + jsonOMDB.Language);
+      console.log("Plot: " + jsonOMDB.Plot);
+      console.log("Actors: " + jsonOMDB.Actors);
+      console.log("Rotton Tomatoes URL: " + jsonOMDB.tomatoURL);
+    }
+  });
+}
+
+function doWhatIsSays() {
+  fs.readFile('random.txt', 'utf8', function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    } else {
+      var command = data.split(',');
+
+      processCommand(command[0], command[1]);
+    }
+  });
+}
+
+processCommand(process.argv[2], process.argv[3]);
